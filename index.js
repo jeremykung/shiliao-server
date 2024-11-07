acbdf3d8d92c112e0e6a7d0b74f9a040901b479c
Jeremy Kung <jeremykung@gmail.com>
2024-11-08 01:45:07 +0200

    added endpoints, improved index and add

--- index.js
+++ index.js
@@ -2,33 +2,51 @@
 import express from "express"
 import bodyParser from "body-parser"
 import morgan from "morgan"
+import pg from "pg"
+import cors from "cors"
 
-// below 3 lines gets path to directory
 import { dirname } from "path"
 import { fileURLToPath } from "url"
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
+// 🟢 Setup
 const app = express()
 const port = 3000
-let fullString = ''
 
-// 🟢 Custom Middleware
-function logger(req, res, next) {
-    console.log('Request Method:', req.method)
-    console.log('Request URL:', req.url)
-    next()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const corsOptions = {
+    origin: "http://localhost:8000"
 }
-function stringJoiner(req, res, next) {
-    console.log(req.body)
-    fullString = req.body.amount + req.body.food
-    next()
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "shiliao",
+    password: "p0stGres",
+    port: 5432
+})
+
+try {
+    const conn = await db.connect();
+    console.log("DB Connection Successful")
+
+    db.query("SELECT * FROM food", (err, res) => {
+        if (err) {
+            console.log("DB error:", err)
+        } else {
+            console.log("DB Food Table:", res.rows)
+        }
+    })
+
+} catch (error) {
+    console.log('DB Connection Error:', error)    
 }
 
 // 🟢 Mount Middleware
-app.use(logger)
 app.use(morgan("dev"))
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(stringJoiner)
 
 // 🟢 Endpoints
 
@@ -45,16 +63,64 @@ app.get("/login", (req, res) => {
     res.sendFile(__dirname + "/public/login.html")
 })
 
-app.post("/login", (req, res) => {
-    console.log(req.body)
-    console.log('fullString:', fullString)
-    res.status(200).send("user loggged in")
+app.get("/all", (req, res) => {
+    let data = []
+    const query = "SELECT * FROM food"
+    db.query(query, (err, dbRes) => {
+        if (err) {
+            console.log("DB Error:", err)
+        } else {
+            console.log("All Foods Data:", dbRes.rows)
+            res.status(200).send(dbRes.rows)
+        }
+    })
 })
 
-app.post("/submit", (req, res) => {
-    console.log(req.body)
-    res.status(201).send(`<h1>Full String:${fullString}</h1>`)
+app.get("/cold", (req, response) => {
+    let data = ['hi']
+    db.query("SELECT * FROM food WHERE property='cold'", (err, res) => {
+        if (err) {
+            console.log("DB error:", err)
+        } else {
+            console.log("DB Food Table:", res.rows)
+            response.send(res.rows)
+        }
+    })
 })
+
+app.post("/add", async (req, response) => {
+    console.log("add food data:", req.body)
+    const foodData = {
+        name: req.body.name.toLowerCase(),
+        type: req.body.type.toLowerCase(),
+        property: req.body.property.toLowerCase()
+    }
+    try {
+        const query = `INSERT INTO food (name, type, property) VALUES ('${foodData.name}', '${foodData.type}', '${foodData.property}')`
+        console.log("query:", query)
+        await db.query(query, (err, res) => {
+            if (err) {
+                console.log("DB error:", err)
+                if (err.code === '23505') {
+                    response.status(409).send("Food already exists")
+                } else {
+                    response.status(500).send("Error adding food")
+                }
+                // Disallow Duplicate Named Foods (case insensitive)    
+                // Check DB restrictions
+                // Return Error
+                // Error message on frontend
+            } else {
+                console.log("successful insertion:", res)
+                response.send(res)
+            }
+        })
+    } catch (error) {
+        console.log('error adding food:', error)
+    }
+
+})
+
 
 app.listen(port, () => {
     console.log("Server running on port 3000.")
